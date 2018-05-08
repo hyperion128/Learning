0661b6057e3c246cbf3afaabf3891aa44b7d6c8d
g.ohenry.mendez <g.ohenry.mendez@accenture.com>
2018-05-08 12:45:32 -0500

    adding a test class

---
+++ src/Challenges/easy/test.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Challenges.easy;
+
+/**
+ *
+ * @author gustavo.armando
+ */
+public class test {
+    
+}
