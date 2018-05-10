a3d381de084a1990e3e72c99a1e4d545d242bb23
g.ohenry.mendez <g.ohenry.mendez@accenture.com>
2018-05-10 13:50:04 -0500

    Se agrega SimpleAdding y mi clase para testear los challenges.

---
+++ src/Challenges/easy/SimpleAdding.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Challenges.easy;
+
+import java.util.*; 
+import java.io.*;
+
+class SimpleAdding {  
+  public static int SimpleAdding(int num) { 
+  
+    // code goes here   
+    /* Note: In Java the return type of a function and the 
+       parameter types being passed are defined, so this return 
+       call must match the return type of the function.
+       You are free to modify the return type. */
+    int tmpnum=0;
+       for (int a=1; a<=num;a++) tmpnum+=a;
+    return tmpnum;
+  } 
+  
+  public static void main (String[] args) {  
+    // keep this function call here     
+    //Scanner s = new Scanner(System.in);
+    //System.out.print(SimpleAdding(s.nextLine())); 
+    //TestChallenge tester = new TestChallenge();
+    TestChallenge.testMyClass(SimpleAdding(12), 78);
+    TestChallenge.testMyClass(SimpleAdding(140), 9870);
+  }   
+  
+}
---
+++ src/Challenges/easy/TestChallenge.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Challenges.easy;
+
+/**
+ *
+ * @author gustavo.armando
+ */
+public final class TestChallenge {
+    public static void testMyClass(String input, String output){
+        if (input.equals(output))System.out.println("Prueba Exitosa, para la entrada ingresada el resultado fue: \""+output+"\" como era esperado.");
+        else System.out.println("Prueba Fallida, la aplicacion produjo: \""+input+" en lugar de: \""+output+"\"");
+    }
+    public static void testMyClass(int input, int output){
+        if (input==output)System.out.println("Prueba Exitosa, para la entrada ingresada el resultado fue: \""+output+"\" como era esperado.");
+        else System.out.println("Prueba Fallida, la aplicacion produjo: \""+input+" en lugar de: \""+output+"\"");
+    }
+}
